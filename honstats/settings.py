854132ca12f04509b2075856e80af2264afb4043
Marko Pađen <markopaden3@gmail.com>
2021-01-09 15:58:40 +0100

    add database

--- honstats/settings.py
+++ honstats/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'honstats.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'honstats',
+        'USER': 'django_test',
+        'PASSWORD': 'django_test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
