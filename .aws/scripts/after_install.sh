277863ad8a4b448520667e7857b525e89640c27c
Marko Pađen <markopaden3@gmail.com>
2021-02-09 20:59:40 +0100

    fix scripts

--- .aws/scripts/after_install.sh
+++ .aws/scripts/after_install.sh
@@ -4,8 +4,8 @@ cd /home/ec2-user/honstats
 rm -rf venv
 /usr/bin/python3 -m virtualenv venv
 ./venv/bin/pip install -r requirements.txt
-./venv/bin/python manage.py migrate -no-input
-./venv/bin/python manage.py collectstatic -no-input
+./venv/bin/python manage.py migrate --noinput
+./venv/bin/python manage.py collectstatic --noinput
 cd ..
 cp .env honstats/honstats/.env
 service supervisord restart
